c669ad3f40c3eb56930b232da961bdc6e40d7ccb
Dave Rooney <daverooneyca@gmail.com>
2018-09-04 16:08:01 -0400

    Test for extracting the station from the METAR code

---
+++ src/test/java/com/saphala/tdd/metar/MetarParseTests.java
@@ -0,0 +1,19 @@
+package com.saphala.tdd.metar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+
+class MetarParseTests {
+
+	@Test
+	void stationShouldBeTheFirstElement() {
+		String rawMetar = "KDTW 210453Z 10006KT 9SM FEW025 BKN055 OVC110 23/20 A2987 RMK AO2 RAE35 SLP111 P0000 T02330200 402830194";
+		
+		String station = rawMetar.split(" ")[0];
+		
+		assertThat(station).isEqualTo("KDTW");
+	}
+
+}
