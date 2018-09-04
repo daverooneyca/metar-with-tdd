f0e3d23c21b838b4dc7f48870f4a2107e30ac92e
Dave Rooney <daverooneyca@gmail.com>
2018-09-04 16:18:25 -0400

    Added test for the reporting time

--- src/test/java/com/saphala/tdd/metar/MetarParseTests.java
+++ src/test/java/com/saphala/tdd/metar/MetarParseTests.java
@@ -7,13 +7,26 @@ import org.junit.jupiter.api.Test;
 
 class MetarParseTests {
 
+	private static final String RAW_METAR = "KDTW 210453Z 10006KT 9SM FEW025 BKN055 OVC110 23/20 A2987 RMK AO2 RAE35 SLP111 P0000 T02330200 402830194";
+
 	@Test
 	void stationShouldBeTheFirstElement() {
-		String rawMetar = "KDTW 210453Z 10006KT 9SM FEW025 BKN055 OVC110 23/20 A2987 RMK AO2 RAE35 SLP111 P0000 T02330200 402830194";
-		
-		String station = rawMetar.split(" ")[0];
+		String station = getItemAt(0);
 		
 		assertThat(station).isEqualTo("KDTW");
 	}
+	
+    @Test 
+    void reportTimeShouldBeTheSecondElement() {
+        String reportTime = getItemAt(1);
+        
+        assertThat(reportTime).isEqualTo("210453Z");
+        
+    }
+
+    private String getItemAt(int index) {
+        return RAW_METAR.split(" ")[index];
+    }
+	
 
 }
