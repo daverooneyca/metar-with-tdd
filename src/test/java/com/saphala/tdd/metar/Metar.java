e1124395350eecfd6f097eb3b9c5c9ac145dcba1
Dave Rooney <daverooneyca@gmail.com>
2018-09-11 15:55:53 -0400

    Added Chicago clouds and refactored into a separate class for the METAR

---
+++ src/test/java/com/saphala/tdd/metar/Metar.java
@@ -0,0 +1,41 @@
+package com.saphala.tdd.metar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Metar {
+
+   public static final int STATION_INDEX = 0;
+   public static final int REPORT_TIME_INDEX = 1;
+   public static final int WINDS_INDEX = 2;
+   public static final int VISIBILITY_INDEX = 3;
+   public static final int CLOUDS_BEGIN_INDEX = 4;
+   public static final int TEMPERATURE_INDEX = 7;
+   public static final int ALTIMETER_INDEX = 8;
+   
+   private String rawMetar;
+   private String[] rawMetarArray;
+
+   public Metar(String metar) {
+      this.rawMetar = metar;
+      this.rawMetarArray = this.rawMetar.split(" ");
+   }
+
+   public List<String> extractClouds() {
+      List<String> clouds = new ArrayList<String>();
+      
+      for (int i = Metar.CLOUDS_BEGIN_INDEX; i < rawMetarArray.length; i++) {
+         if(Character.isDigit(rawMetarArray[i].charAt(0))) {
+            break;
+         }
+         
+         clouds.add(rawMetarArray[i]);
+      }
+      
+      return clouds;
+   }
+
+   public String getItemAt(int index) {
+      return rawMetarArray[index];
+   }
+}
--- src/test/java/com/saphala/tdd/metar/MetarParseTests.java
+++ src/test/java/com/saphala/tdd/metar/MetarParseTests.java
@@ -2,139 +2,123 @@ package com.saphala.tdd.metar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 class MetarParseTests {
 
-   private static final int STATION_INDEX = 0;
-   private static final int REPORT_TIME_INDEX = 1;
-   private static final int WINDS_INDEX = 2;
-   private static final int VISIBILITY_INDEX = 3;
-   private static final int CLOUDS_BEGIN_INDEX = 4;
-   private static final int TEMPERATURE_INDEX = 7;
-   private static final int ALTIMETER_INDEX = 8;
 
    private static final String METAR_DETROIT = "KDTW 210453Z 10006KT 9SM FEW025 BKN055 OVC110 23/20 A2987 RMK AO2 RAE35 SLP111 P0000 T02330200 402830194";
    private static final String METAR_CHICAGO = "KORD 111751Z VRB04KT 10SM FEW043 25/13 A3016 RMK AO2 SLP209 T02500128 10250 20144 58003";
    
-   private String metar = METAR_DETROIT;
-   
-   
+
    @Test
    void stationForDetroit() {
-      String station = getItemAt(STATION_INDEX);
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      String station = metar.getItemAt(Metar.STATION_INDEX);
 
       assertThat(station).isEqualTo("KDTW");
    }
 
    @Test
    void reportTimeForDetroit() {
-      String reportTime = getItemAt(REPORT_TIME_INDEX);
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      String reportTime = metar.getItemAt(Metar.REPORT_TIME_INDEX);
 
       assertThat(reportTime).isEqualTo("210453Z");
    }
    
    @Test
    void windsForDetroit() {
-      String winds = getItemAt(WINDS_INDEX);
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      String winds = metar.getItemAt(Metar.WINDS_INDEX);
       
       assertThat(winds).isEqualTo("10006KT");
    }
    
    @Test
    void visibilityForDetroit() {
-      String visibility = getItemAt(VISIBILITY_INDEX);
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      String visibility = metar.getItemAt(Metar.VISIBILITY_INDEX);
             
       assertThat(visibility).isEqualTo("9SM");
    }
    
    @Test
    void cloudsForDetroit() {
-      List<String> clouds = extractClouds();
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      List<String> clouds = metar.extractClouds();
       
       assertThat(clouds).containsExactly("FEW025", "BKN055", "OVC110");
    }
    
    @Test
    void temperatureForDetroit() {
-      String temperature = getItemAt(TEMPERATURE_INDEX);
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      String temperature = metar.getItemAt(Metar.TEMPERATURE_INDEX);
       
       assertThat(temperature).isEqualTo("23/20");
    }
    
    @Test
    void altimeterForDetroit() {
-      String altimeter = getItemAt(ALTIMETER_INDEX);
+      Metar metar = new Metar(METAR_DETROIT);
+      
+      String altimeter = metar.getItemAt(Metar.ALTIMETER_INDEX);
       
       assertThat(altimeter).isEqualTo("A2987");
    }
    
    @Test
    void stationForChicago() {
-      metar = METAR_CHICAGO;
+      Metar metar = new Metar(METAR_CHICAGO);
       
-      String station = getItemAt(STATION_INDEX);
+      String station = metar.getItemAt(Metar.STATION_INDEX);
       
       assertThat(station).isEqualTo("KORD");
    }
    
    @Test
    void reportTimeForChicago() {
-      metar = METAR_CHICAGO;
+      Metar metar = new Metar(METAR_CHICAGO);
       
-      String reportTime = getItemAt(REPORT_TIME_INDEX);
+      String reportTime = metar.getItemAt(Metar.REPORT_TIME_INDEX);
       
       assertThat(reportTime).isEqualTo("111751Z");
    }
    
    @Test
    void windsForChicago() {
-      metar = METAR_CHICAGO;
+      Metar metar = new Metar(METAR_CHICAGO);
       
-      String winds = getItemAt(WINDS_INDEX);
+      String winds = metar.getItemAt(Metar.WINDS_INDEX);
       
       assertThat(winds).isEqualTo("VRB04KT");
    }
    
    @Test
    void visibilityforChicago() {
-      metar = METAR_CHICAGO;
+      Metar metar = new Metar(METAR_CHICAGO);
       
-      String visibility = getItemAt(VISIBILITY_INDEX);
+      String visibility = metar.getItemAt(Metar.VISIBILITY_INDEX);
       
       assertThat(visibility).isEqualTo("10SM");
    }
    
    @Test
    void cloudsForChicago() {
-      metar = METAR_CHICAGO;
+      Metar metar = new Metar(METAR_CHICAGO);
       
-      List<String> clouds = extractClouds();
+      List<String> clouds = metar.extractClouds();
       
       assertThat(clouds).containsExactly("FEW043"); 
-   }
-
-   private List<String> extractClouds() {
-      List<String> clouds = new ArrayList<String>();
-      
-      String[] rawMetarArray = metar.split(" ");
-      
-      for (int i = CLOUDS_BEGIN_INDEX; i < rawMetarArray.length; i++) {
-         if(Character.isDigit(rawMetarArray[i].charAt(0))) {
-            break;
-         }
-         
-         clouds.add(rawMetarArray[i]);
-      }
-      
-      return clouds;
-   }
-   
-   private String getItemAt(int index) {
-      return metar.split(" ")[index];
    }
 
 }
